26c343d891ae38e8a16fc9b115a5ec0b1bfaf362
RafaelGSS <rafael.nunu@hotmail.com>
2019-03-25 09:29:41 -0300

    added base pf search!

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 require('dotenv').config()
 // Init connection with database
 const app = require('express')()
+const cors = require('cors')
 
 const port = process.env.PORT || 4000
 
 const routes = require('./src/routes/routes')
 const middlewares = require('./src/routes/middlewares')
 
+app.use(cors())
 app.use(middlewares)
 app.use(routes)
 
